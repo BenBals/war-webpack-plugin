f1b0ce0944e41fc0ab2dbdd32fb7744e13b7062e
BenBals <benjustusbals@gmail.com>
2017-06-29 18:46:10 +0200

    add missing underscore to function name

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ WarPlugin.prototype._generateUrlRewriteXmlBuffer = function () {
   return Buffer.from(urlrewriteXml, 'utf8');
 }
 
-WarPlugin.prototype.generateWebXmlBuffer = function () {
+WarPlugin.prototype._generateWebXmlBuffer = function () {
   // Careful, no blank space before <?xml
           const webXml = `<?xml version="1.0" encoding="UTF-8"?>
             <web-app xmlns="http://xmlns.jcp.org/xml/ns/javaee" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://xmlns.jcp.org/xml/ns/javaee http://xmlns.jcp.org/xml/ns/javaee/web-app_4_0.xsd" version="4.0" metadata-complete="true">
